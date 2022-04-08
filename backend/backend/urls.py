c1bdb633f14f820a69e0f51b01d34cd07ff8129a
Viacheslav Artemenkov <vchslvartemenkov@gmail.com>
2022-04-08 19:43:04 +0300

    add user app

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+from users.views import UserModelViewSet
+
+router = DefaultRouter()
+router.register('users', UserModelViewSet)
 
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
 ]
--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,15 @@
+import uuid
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    user_name = models.CharField(max_length=255)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+    email = models.EmailField(max_length=255, unique=True)
+
+    create_at = models.DateField(auto_now_add=True)
+    update_at = models.DateField(auto_now=True)
