813ba06fc8f0862a572372b525c1a982b03c49a2
Viacheslav Artemenkov <vchslvartemenkov@gmail.com>
2022-04-16 21:46:04 +0300

    backend: migrations

--- backend/todo/models.py
+++ backend/todo/models.py
@@ -1,22 +1,22 @@
 import uuid
 from django.db import models
 
-from backend.users.models import User
+from users.models import User
 
 # Create your models here.
 
 class Project(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(max_length=255, required=True)
+    name = models.CharField(max_length=255, null=False, blank=False)
     repository_link = models.CharField(max_length=255)
     users = models.ManyToManyField(User)
     is_deleted = models.BooleanField(default=False)
 
 
 class ToDo(models.Model):
-    project = models.ForeignKey(Project, required=True, on_delete=models.CASCADE, on_update=models.CASCADE)
+    project = models.ForeignKey(Project, null=False, blank=False, on_delete=models.CASCADE)
     text = models.TextField(blank=True)
-    user = models.ForeignKey(User, required=True, on_delete=models.CASCADE, on_update=models.CASCADE)
+    user = models.ForeignKey(User, null=False, on_delete=models.CASCADE)
     
     create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
