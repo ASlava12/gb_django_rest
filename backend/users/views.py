ad73924806791044e0db99c25e8d22101ee4cf3f
Viacheslav Artemenkov <vchslvartemenkov@gmail.com>
2022-04-19 18:26:41 +0300

    add pagination

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,8 +1,18 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.mixins import ListModelMixin, UpdateModelMixin, RetrieveModelMixin
+from rest_framework.viewsets import GenericViewSet
+# from rest_framework.generics import ListAPIView, UpdateAPIView
 from .models import User
 from .serializers import UserSerializer
+from rest_framework.pagination import LimitOffsetPagination
+
 
 # Create your views here.
-class UserModelViewSet(ModelViewSet):
-    queryset = User.objects.all()
+class UserLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 25
+
+
+class UserModelViewSet(ListModelMixin, UpdateModelMixin, RetrieveModelMixin, GenericViewSet):
+    pagination_class = UserLimitOffsetPagination
+    queryset = User.objects.filter(is_deleted=False)
     serializer_class = UserSerializer
